033a64b88ebe78858df59cdad5fb9541e84a3fc4
Mandy8055 <msankrit8055@gmail.com>
2021-04-18 13:42:07 +0530

    Added

--- mainRunner4.py
+++ mainRunner4.py
@@ -1,36 +1,49 @@
-# Should be merged in mainRunner.py
-
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from skimage.filters import threshold_local
 from skimage import measure
 
-
-image = cv2.imread('data/images/test5.jpg')
+image = cv2.imread('data/images/pot.jpg')
 print("Height: {},Width: {}".format(*image.shape[:2]))
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # plt.imshow(gray, cmap='gray')
 
+# kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+# blurred = cv2.filter2D(gray , -1, kernel)
+# blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 blurred = cv2.bilateralFilter(gray, 21, 41, 41)
 # plt.imshow(blurred)
 
-# Watch for the argument
-# For test5.jpg, Canny arguments 50, 200
-edged = cv2.Canny(blurred, 50, 200)
-# plt.imshow(edged)
+edged = cv2.Canny(blurred, 30, 190, apertureSize=3, L2gradient=True)
+# 30, 190 --> pot
+# 60, 220 --> guber
+plt.imshow(edged)
 
+minLineLength = 100
+count = 0
+lines = cv2.HoughLinesP(image=edged, rho=1, theta=np.pi/180, threshold=100,
+                        lines=np.array([]), minLineLength=minLineLength, maxLineGap=90)
+
+a, b, c = lines.shape
+for i in range(a):
+    cv2.line(gray, (lines[i][0][0], lines[i][0][1]), (lines[i]
+             [0][2], lines[i][0][3]), (0, 0, 255), 3, cv2.LINE_AA)
+    count = count + 1
+print("count ", count)
+cv2.imwrite('houghlines6.jpg', gray)
+
+
+# Experimental setup
 thresh = threshold_local(blurred, 21, offset=15).astype("uint8")*255
 mask = cv2.bitwise_not(edged)
 thresh = cv2.bitwise_and(thresh, thresh, mask=mask)
-# For test5.jpg, structuring element kernel is 8x8
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (8, 8))
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 25))
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
 # plt.imshow(thresh)
 thresh_copy = thresh.copy()
-(cnts, _) = cv2.findContours(thresh_copy,
-                             cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+(cnts, _) = cv2.findContours(thresh_copy, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 output = image.copy()
 # for c in cnts:
@@ -38,16 +51,15 @@ output = image.copy()
 #     output = cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
 # plt.imshow(output)
 for c in cnts:
-    cv2.drawContours(output, c, -1, (255, 255, 255), 2)
+    cv2.drawContours(output, c, -1, (0, 255, 0), 2)
 # plt.imshow(output)
+# Jump to line
 
 print("# of bricks:", len(cnts))
 cv2.imshow("Bricks", output)
 cv2.waitKey(0)
 
-# Connected component analysis
-
-num_labels, labels_im = cv2.connectedComponents(thresh, connectivity=8)
+num_labels, labels_im = cv2.connectedComponents(thresh, connectivity=4)
 labels = measure.label(thresh, background=0)
 mask = np.zeros(thresh.shape, dtype="uint8")
 
@@ -61,5 +73,6 @@ for (i, label) in enumerate(np.unique(labels_im)):
 
     mask = cv2.add(mask, labelMask)
     cv2.imshow("Mask", mask)
-    cv2.waitKey(0)
+    # cv2.waitKey(0)
 print("# of bricks (accurate):", len(np.unique(labels_im)) - 1)
+cv2.waitKey(0)
