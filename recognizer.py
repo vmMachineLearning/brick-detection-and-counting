387365a57d3f8e2cdcb7ecb17ffeb02370cb4a37
Mandy8055 <msankrit8055@gmail.com>
2021-04-19 12:17:56 +0530

    Updated the file structure and performed some housekeeping

---
+++ recognizer.py
@@ -0,0 +1,59 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from skimage.filters import threshold_local
+from skimage import measure
+
+
+def countBricks(image, cannyThreshMin, cannyThreshMax, kernelSize):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.bilateralFilter(gray, 21, 41, 41)
+    edged = cv2.Canny(blurred, cannyThreshMin, cannyThreshMax,
+                      apertureSize=3)
+    # 30, 190 --> pot
+    # 60, 220 --> guber
+    # minLineLength = 100
+    # count = 0
+    # lines = cv2.HoughLinesP(image=edged, rho=1, theta=np.pi/180, threshold=100,
+    #                         lines=np.array([]), minLineLength=minLineLength, maxLineGap=90)
+
+    # a, b, c = lines.shape
+    # for i in range(a):
+    #     cv2.line(gray, (lines[i][0][0], lines[i][0][1]), (lines[i]
+    #                                                       [0][2], lines[i][0][3]), (0, 0, 255), 3, cv2.LINE_AA)
+    #     count = count + 1
+    # print("count ", count)
+    # cv2.imwrite('houghlines6.jpg', gray)
+    thresh = threshold_local(blurred, 21, offset=15).astype("uint8")*255
+    mask = cv2.bitwise_not(edged)
+    thresh = cv2.bitwise_and(thresh, thresh, mask=mask)
+    kernel = cv2.getStructuringElement(
+        cv2.MORPH_RECT, (kernelSize, kernelSize))
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    thresh_copy = thresh.copy()
+    (cnts, _) = cv2.findContours(thresh_copy,
+                                 cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    output = image.copy()
+    for c in cnts:
+        cv2.drawContours(output, c, -1, (0, 255, 0), 2)
+    print("# of bricks:", len(cnts))
+    cv2.imshow("Bricks", output)
+    cv2.waitKey(0)
+
+    num_labels, labels_im = cv2.connectedComponents(thresh, connectivity=4)
+    labels = measure.label(thresh, background=0)
+    mask = np.zeros(thresh.shape, dtype="uint8")
+
+    for (i, label) in enumerate(np.unique(labels_im)):
+        if label == 0:
+            continue
+
+        labelMask = np.zeros(thresh.shape, dtype="uint8")
+        labelMask[labels_im == label] = 255
+        numPixels = cv2.countNonZero(labelMask)
+
+        mask = cv2.add(mask, labelMask)
+        cv2.imshow("Mask", mask)
+        cv2.waitKey(0)
+    print("# of bricks (accurate):", len(np.unique(labels_im)) - 1)
+    # cv2.waitKey(0)
